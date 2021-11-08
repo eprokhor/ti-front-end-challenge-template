1c73e0d7b713e155a62f42486da79365e93ea00f
Eduard Prokhor <61ue5peed@gmail.com>
2021-11-07 23:26:17 -0800

    can't get it figured out.

--- src/components/Table.test.tsx
+++ src/components/Table.test.tsx
@@ -1,7 +1,15 @@
 import Table from './Table';
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
+import { mount, ReactWrapper } from 'enzyme';
+import { isGetAccessorDeclaration } from 'typescript';
 
 describe('Table', () => {
+  let wrapper: ReactWrapper<any, any, any>;
+
+  beforeEach(() => {
+    wrapper = mount(<Table />);
+  });
+
   it('stock rows exists', async () => {
     const { getByText, container } = render(<Table />);
 
@@ -16,10 +24,32 @@ describe('Table', () => {
     expect(cells).toHaveLength(4);
   });
 
-  it('should load data from api', () => {
-    // const component = mount(<Table />);
-    // would use :
-    //https://stackoverflow.com/questions/55388587/how-should-i-test-react-hook-useeffect-making-an-api-call-with-typescript
-    //but had dependancy
+  it('should load data from api', (done: () => void) => {
+    console.log(wrapper.debug());
+
+    expect(wrapper).not.toBeNull();
+
+    setImmediate(() => {
+      wrapper.update();
+      console.log(wrapper.debug());
+      done();
+    });
+
+    // wrapper.invoke('useEffect')();
+    // act(() => wrapper.prop('row')());
+    // wrapper.update();
+    // console.log(wrapper.debug());
   });
+
+  it('try again', async () => {
+    await waitForComponentToPaint(wrapper);
+    console.log(wrapper.debug());
+  });
+
+  const waitForComponentToPaint = async (wrapper: { update: () => void }) => {
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 0));
+      wrapper.update();
+    });
+  };
 });
